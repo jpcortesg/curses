1c50ca84d2eb3e4c132c9a77c76e51fa3f3a5334
jpcortesg <juan2001cortes.jc@gmail.com>
2020-07-21 03:48:49 -0500

    starting to implement authorization and authentication

--- developing/serverless/api/index.js
+++ developing/serverless/api/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors') 
 const meals = require('./routes/meals')
 const orders = require('./routes/orders')
+const auth = require('./routes/auth')
 
 const app = express()
 app.use(bodyParser.json())
@@ -13,5 +14,6 @@ mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopo
 
 app.use('/api/meals', meals)
 app.use('/api/orders', orders)
+app.use('/api/auth', auth)
 
 module.exports = app
---
+++ developing/serverless/api/routes/auth.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const crypto = require('crypto')
+const jwt = require('jsonwebtoken')
+const Users = require('../models/Users')
+
+const router = express.Router()
+
+const signToken = (_id) => {
+  return jwt.sign({ _id }, 'My secret', {
+    expiresIn : 60 * 60 * 24 * 365,
+  })
+}
+
+router.post('/register', (req, res) => {
+  const {email, password} = req.body
+  crypto.randomBytes(16, (err, salt) => {
+    const nweSalt = salt.toString('base64')
+    crypto.pbkdf2(password, nweSalt, 10000, 64, 'sha1', (err, key) => {
+      const encryptedPassword = key.toString('base64')
+      Users.findOne({ email }).exec()
+        .then(user => {
+          if(user){
+            return res.send('User already exists')
+          }
+          Users.create({
+            email,
+            password: encryptedPassword,
+            salt: nweSalt
+          }).then(() => {
+            res.send('User created with exit')
+          })
+        })
+    })
+  })
+})
+
+router.post('/login', (req, res) => {
+  const { email, password } = req.body
+  Users.findOne({ email }).exec()
+    .then(user => {
+      if(!user){
+        return res.send('Incorrect username and/or password')
+      }
+      crypto.pbkdf2(password, user.salt, 10000, 64, 'sha1', (err, key) => {
+        const encryptedPassword = key.toString('base64')
+        if(user.password === encryptedPassword){
+          const token = signToken(user._id)
+          return res.send({ token })
+        }
+        res.send('Incorrect username and/or password')
+      })
+    })
+})
+
+module.exports = router
