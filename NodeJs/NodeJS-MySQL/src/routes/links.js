ce8cdcc651c0e78b0de46ff1b82b9f1d650de7d8
jpcortesg <juan2001cortes.jc@gmail.com>
2020-09-19 21:13:06 -0500

    delete links of page

--- NodeJs/NodeJS-MySQL/src/routes/links.js
+++ NodeJs/NodeJS-MySQL/src/routes/links.js
@@ -15,7 +15,18 @@ router.post('/add', async (req, res) => {
     description
   }
   await pool.query('INSERT INTO links set ?', [newLink])
-  res.send('Received')
+  res.redirect('/links')
+})
+
+router.get('/', async(req, res) => {
+  const links = await pool.query('SELECT * FROM links')
+  res.render('links/list', { links })
+})
+
+router.get('/delete/:id', async (req, res) => {
+  const { id } = req.params 
+  await pool.query('DELETE FROM links where id = ?', [id])
+  res.redirect('/links')
 })
 
 module.exports = router
