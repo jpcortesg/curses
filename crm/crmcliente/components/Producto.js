490b8f2a02fc58990862a7900731c8ccc9fd7135
jpcortesg <juan2001cortes.jc@gmail.com>
2021-01-13 19:34:35 -0500

    update product in crm

--- crm/crmcliente/components/Producto.js
+++ crm/crmcliente/components/Producto.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Swal from 'sweetalert2'
 import { gql, useMutation } from '@apollo/client'
+import Router from 'next/router'
 
 const ELIMINAR_PRODUCTO = gql`
   mutation eliminarProducto($id: ID!){
@@ -71,6 +72,13 @@ const Producto = ({producto}) => {
     })
   }
 
+  const editarProducto = () => {
+    Router.push({
+      pathname: '/editarProducto/[id]',
+      query: { id }
+    })
+  }
+
   return (
     <tr>
       <td className='border px-4 py-2'>{nombre}</td>
@@ -88,7 +96,7 @@ const Producto = ({producto}) => {
       <td className="border px-4 py-2">
         <button type='button'
           className='flex justify-center items-center bg-green-600 py-2 px-4  w-full text-white uppercase rounded font-bold text-xs'
-          // onClick={ () => editarCliente() }
+          onClick={ () => editarProducto() }
         >
           Editar
           <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
